14c777f456dbdbf6fae37243595416429b6ae4ab
José Solis <jose.solis@globant.com>
2024-03-27 13:05:14 -0300

    delete comments

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
-dotenv.config(); // Cargar las variables de entorno
+dotenv.config(); 
 
-const PORT = process.env.PORT || 8080; // Puedes configurar el puerto también como una variable de entorno
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 const apiUrl = process.env.API_URL; 
@@ -15,11 +15,10 @@ const options = {
   headers: {
     'Content-Type': 'application/json',
     'x-origin-client': 'mike:hogeboom:postman',
-    'Authorization' : `Bearer ${process.env.AUTHORIZATION_BEARER}` // Usar la variable de entorno
+    'Authorization' : `Bearer ${process.env.AUTHORIZATION_BEARER}`
   },
 };
 
-// Si necesitas configuraciones sin autorización, puedes mantenerlas como están o también gestionarlas mediante variables de entorno
 const unauthorizedOptions = {
   method: 'GET', 
   headers: {
@@ -31,7 +30,6 @@ const unauthorizedOptions = {
 app.use(cors());
 
 app.get('/api/proxy', async (req, res) => {
-    // Usar options o unauthorizedOptions basado en tu lógica
     try {
       const response = await fetch(apiUrl, options); // Asegúrate de usar la variable correcta aquí
       const data = await response.json();
